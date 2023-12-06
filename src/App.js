f61dda9dfc0de7af55e6bf0a70e2c393e4d04a68
Stephen Walker <35walke774@gmail.com>
2023-12-05 19:13:14 -0500

    updated version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "../src/App.css";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import UserProfile from "./UserProfile";
+
+function Home() {
+  return <p>Home</p>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div>
+          <Link to="/">Home</Link>
+        </div>
+        {Array(10)
+          .fill()
+          .map((ignoredValue, index) => index + 1)
+          .map((id) => (
+            <div key={id}>
+              <Link to={`/user/${id}`}>User {id}</Link>
+            </div>
+          ))}
+        <Switch>
+          <Route exact={true} path="/">
+            <Home />
+          </Route>
+          <Route path="/user/:userId">
+            <UserProfile />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
